887953bf3e6ab988dfa93ba7ecd5595e8a79e6df
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-08-22 17:07:49 +0900

    - feature: enable sidebar to fold or unfold

--- frontend/src/components/Common/Sidebar.tsx
+++ frontend/src/components/Common/Sidebar.tsx
@@ -1,92 +1,14 @@
-import { Box, Flex, IconButton, Text } from "@chakra-ui/react"
-import { useQueryClient } from "@tanstack/react-query"
-import { useState } from "react"
-import { FaBars } from "react-icons/fa"
-import { FiLogOut } from "react-icons/fi"
-
-import type { UserPublic } from "@/client"
-import { useWebSocket } from "@/contexts/WebSocketContext"
-import useAuth from "@/hooks/useAuth"
-import {
-  DrawerBackdrop,
-  DrawerBody,
-  DrawerCloseTrigger,
-  DrawerContent,
-  DrawerRoot,
-  DrawerTrigger,
-} from "../ui/drawer"
+import { Box } from "@chakra-ui/react"
 import SidebarItems from "./SidebarItems"
 
 const Sidebar = () => {
-  const queryClient = useQueryClient()
-  const currentUser = queryClient.getQueryData<UserPublic>(["currentUser"])
-  const { logout } = useAuth()
-  const { disconnect } = useWebSocket()
-  const [open, setOpen] = useState(false)
-
-  const handleLogout = () => {
-    disconnect() // WebSocket 연결 종료
-    logout()
-  }
-
   return (
     <>
-      {/* Mobile */}
-      <DrawerRoot
-        placement="start"
-        open={open}
-        onOpenChange={(e) => setOpen(e.open)}
-      >
-        <DrawerBackdrop />
-        <DrawerTrigger asChild>
-          <IconButton
-            variant="ghost"
-            color="inherit"
-            display={{ base: "flex", md: "none" }}
-            aria-label="Open Menu"
-            position="absolute"
-            zIndex="100"
-            m={4}
-          >
-            <FaBars />
-          </IconButton>
-        </DrawerTrigger>
-        <DrawerContent maxW="xs">
-          <DrawerCloseTrigger />
-          <DrawerBody>
-            <Flex flexDir="column" justify="space-between">
-              <Box>
-                <SidebarItems onClose={() => setOpen(false)} />
-                <Flex
-                  as="button"
-                  onClick={handleLogout}
-                  alignItems="center"
-                  gap={4}
-                  px={4}
-                  py={2}
-                >
-                  <FiLogOut />
-                  <Text>Log Out</Text>
-                </Flex>
-              </Box>
-              {currentUser?.email && (
-                <Text fontSize="sm" p={2} truncate maxW="sm">
-                  Logged in as: {currentUser.email}
-                </Text>
-              )}
-            </Flex>
-          </DrawerBody>
-          <DrawerCloseTrigger />
-        </DrawerContent>
-      </DrawerRoot>
-
-      {/* Desktop */}
       <Box
         display={{ base: "none", md: "flex" }}
         position="sticky"
         bg="bg.subtle"
         top={0}
-        minW="12vw"
         h="100vh"
         p={4}
       >
--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,12 +1,13 @@
-import { Box, Flex, Icon, Text } from "@chakra-ui/react"
+import { Box, Flex, Icon, IconButton, Text } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link as RouterLink } from "@tanstack/react-router"
-import { FaRobot } from "react-icons/fa"
+import { FaBars, FaRobot } from "react-icons/fa"
 import { FaLink } from "react-icons/fa"
 import { FiHome, FiSettings, FiUsers } from "react-icons/fi"
 import type { IconType } from "react-icons/lib"
 
 import type { UserPublic } from "@/client"
+import { useState } from "react"
 
 const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
@@ -29,6 +30,8 @@ const SidebarItems = ({ onClose }: SidebarItemsProps) => {
   const queryClient = useQueryClient()
   const currentUser = queryClient.getQueryData<UserPublic>(["currentUser"])
 
+  const [fold, setFold] = useState(true)
+
   const finalItems: Item[] = currentUser?.is_superuser
     ? [...items, { icon: FiUsers, title: "Admin", path: "/admin" }]
     : items
@@ -39,24 +42,61 @@ const SidebarItems = ({ onClose }: SidebarItemsProps) => {
         gap={4}
         px={4}
         py={2}
+        minH="35px"
         _hover={{
           background: "gray.subtle",
         }}
         alignItems="center"
+        justifyContent="flex-start"
         fontSize="sm"
       >
-        <Icon as={icon} alignSelf="center" />
-        <Text ml={2}>{title}</Text>
+        <Icon
+          as={icon}
+          boxSize="16px"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        />
+        {!fold && <Text>{title}</Text>}
       </Flex>
     </RouterLink>
   ))
 
   return (
     <>
-      <Text fontSize="xs" px={4} py={2} fontWeight="bold">
-        Menu
-      </Text>
-      <Box>{listItems}</Box>
+      <Flex>
+        {fold ? (
+          <IconButton
+            onClick={() => setFold(false)}
+            variant="ghost"
+            px={3.5}
+            py={2}
+            minH="35px"
+            color="inherit"
+          >
+            <Icon as={FaBars} alignSelf="center" />
+          </IconButton>
+        ) : (
+          <>
+            <Flex flex="1" justifyContent="flex-start" alignItems="center">
+              <Text fontSize="xs" px={4} py={2} fontWeight="bold">
+                Menu
+              </Text>
+            </Flex>
+            <IconButton
+              onClick={() => setFold(true)}
+              variant="ghost"
+              px={3}
+              py={2}
+              minH="35px"
+              color="inherit"
+            >
+              <Icon as={FaBars} alignSelf="center" />
+            </IconButton>
+          </>
+        )}
+      </Flex>
+      <Box minW={fold ? "0" : "10vw"}>{listItems}</Box>
     </>
   )
 }
