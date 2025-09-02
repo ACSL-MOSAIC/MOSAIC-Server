3b25391036e348bc8b076e20078a62e64f43905f
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-09-02 11:04:06 +0900

    - bug: fix bug with update robot status

--- backend/app/schemas/robot.py
+++ backend/app/schemas/robot.py
@@ -36,6 +36,7 @@ class RobotCreate(RobotBase):
 class RobotUpdate(SQLModel):
     name: str | None = Field(default=None, min_length=1, max_length=255)
     description: str | None = Field(default=None, max_length=255)
+    status: RobotStatus | None = None
 
 
 # Database model, database table inferred from class name
